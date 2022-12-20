0c85e8b39b0b9525645dae210f1134fae652687f
Remi <remi.nkjm@gmail.com>
2022-12-20 16:41:06 +0900

    Write and Writeline

--- Sample101/Program.cs
+++ Sample101/Program.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sample101 
+namespace Sample101 // Project's name
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            /* Block comments*/
+            // 1 line comment
+
+            // Writeでは改行されない
+            Console.Write(123);
+            Console.WriteLine(456);
+            // Writelineでは改行される
+            Console.Write("ABC");
+            Console.WriteLine("DEF"); 
         }
     }
 }
